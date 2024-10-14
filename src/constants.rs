f028253f4cfb612c7af4dbee9a77610e77e2be53
Pedro Piquero <gowikel@gmail.com>
2024-10-14 17:24:05 +0100

    Add AOC_COOKIE constant to constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -6,4 +6,5 @@ pub const VALID_YEARS: RangeInclusive<u32> = 2023..=2023;
 /// December days that contain a puzzle
 pub const VALID_DAYS: RangeInclusive<u32> = 1..=25;
 
+/// The require AOC_COOKIE
 pub const AOC_COOKIE: &str = "AOC_COOKIE";
