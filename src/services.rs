d83cc9f1ad33fbb9093667152ab766eaf716cf2a
Pedro Piquero <gowikel@gmail.com>
2024-10-28 23:01:41 +0000

    Add documentation for services module

--- src/services.rs
+++ src/services.rs
@@ -1,3 +1,16 @@
+//! The services module provides high-level abstractions for file system operations
+//! and HTTP operations required for the Advent of Code puzzles.
+//!
+//! It re-exports the FSService and HTTPService structs from their respective modules.
+//!
+//! # Overview
+//!
+//! This module encapsulates two core services:
+//! * FSService: Manages file system operations, such as extracting and writing puzzle
+//! templates.
+//! * HTTPService: Manages HTTP operations, such as downloading puzzle inputs from the
+//!   Advent of Code website.
+
 pub mod fs;
 pub mod http;
 
