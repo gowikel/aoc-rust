567f67854961daf46337908186854e8039530e7d
Pedro Piquero <gowikel@gmail.com>
2024-11-25 02:14:55 +0000

    Solve Y2023/D05P02

--- src/solvers/y2023/day05.rs
+++ src/solvers/y2023/day05.rs
@@ -3,8 +3,9 @@ use crate::{
     solvers::{Solution, SolutionExecution},
     Execute,
 };
-use log::{debug, trace};
-use std::collections::{HashMap, HashSet};
+use log::trace;
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::fmt::{Debug, Formatter};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
@@ -27,9 +28,19 @@ pub fn solve(execute: Execute, input_path: &Path) -> [Solution; 2] {
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone, Copy)]
 struct AlmanacEntry(u64, u64, u64);
 
+#[derive(Eq, PartialOrd, Ord, PartialEq, Clone, Copy)]
+struct SeedRange(u64, u64);
+
+impl Debug for SeedRange {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}..{}", self.0, self.0 + self.1 - 1)
+    }
+}
+
 #[derive(Debug, Eq, PartialEq, Clone)]
 struct FileData {
     seeds: Vec<u64>,
+    seed_ranges: Vec<SeedRange>,
     almanac: HashMap<String, Vec<AlmanacEntry>>,
     almanac_keys: HashMap<String, String>,
     destination: HashMap<String, String>,
@@ -40,6 +51,7 @@ impl FileData {
     fn new() -> Self {
         FileData {
             seeds: Vec::new(),
+            seed_ranges: Vec::new(),
             almanac: HashMap::new(),
             almanac_keys: HashMap::new(),
             destination: HashMap::new(),
@@ -69,6 +81,47 @@ impl FileData {
     fn add_destination(&mut self, entity: String, destination: String) {
         self.destination.insert(entity, destination);
     }
+
+    fn add_seed_range(&mut self, range: SeedRange) {
+        self.seed_ranges.push(range);
+    }
+
+    fn apply(&self, seed: u64) -> Result<u64, String> {
+        let mut current = "seed";
+        let mut result = seed;
+
+        while current != "location" {
+            let current_key = self
+                .almanac_keys
+                .get(current)
+                .ok_or(format!("No key for {current}"))?;
+
+            let current_entries = self
+                .almanac
+                .get(current_key)
+                .ok_or(format!("No almanac entries for {current}"))?;
+
+            let current_destination = self
+                .destination
+                .get(current_key)
+                .ok_or(format!("No destination for {current}"))?;
+
+            current = current_destination;
+            for entry in current_entries {
+                let source = entry.0;
+                let destination = entry.1;
+                let range_length = entry.2;
+
+                if result >= source && result <= source + range_length - 1 {
+                    result =
+                        destination.wrapping_sub(source).wrapping_add(result);
+                    break;
+                }
+            }
+        }
+
+        Ok(result)
+    }
 }
 
 fn parse_file(input_path: &Path) -> Result<FileData, String> {
@@ -87,10 +140,19 @@ fn parse_file(input_path: &Path) -> Result<FileData, String> {
         }
 
         if line.starts_with("seeds: ") {
-            line[7..]
+            let seeds: Vec<u64> = line[7..]
                 .split_whitespace()
                 .filter_map(|s| s.parse().ok())
-                .for_each(|s| result.add_seed(s));
+                .collect();
+
+            seeds.iter().for_each(|s| result.add_seed(*s));
+
+            seeds.chunks_exact(2).for_each(|chunk| {
+                let seed_start = chunk[0];
+                let seed_length = chunk[1];
+
+                result.add_seed_range(SeedRange(seed_start, seed_length));
+            });
 
             continue;
         }
@@ -144,51 +206,19 @@ fn parse_file(input_path: &Path) -> Result<FileData, String> {
 }
 
 fn solve_part1(input_path: &Path) -> Result<SolutionExecution, String> {
-    let file_data = parse_file(input_path).map_err(|e| e.to_string())?;
+    let file_data = parse_file(input_path)?;
 
-    let mut data = file_data.seeds.clone();
-    let mut current = "seed";
+    let locations: Result<Vec<u64>, String> = file_data
+        .seeds
+        .iter()
+        .map(|seed| file_data.apply(*seed))
+        .collect();
+    let locations = locations?;
 
-    while current != "location" {
-        let current_key = file_data
-            .almanac_keys
-            .get(current)
-            .ok_or(format!("No key for {current}"))?;
-        let current_entries = file_data
-            .almanac
-            .get(current_key)
-            .ok_or(format!("No almanac entries for {current}"))?;
-        let current_destination = file_data
-            .destination
-            .get(current_key)
-            .ok_or(format!("No destination for {current}"))?;
-
-        data = data
-            .iter()
-            .map(|&v| {
-                for entry in current_entries {
-                    let source = entry.0;
-                    let destination = entry.1;
-                    let range_length = entry.2;
-
-                    if v >= source && v <= source + range_length {
-                        return destination
-                            .wrapping_sub(source)
-                            .wrapping_add(v);
-                    }
-                }
-
-                v
-            })
-            .collect();
-
-        current = current_destination;
-    }
-
-    let result = data
+    let result = locations
         .iter()
         .min()
-        .expect("At least one location has to exist");
+        .expect("At least one location should exists");
 
     Ok(SolutionExecution::Value(*result))
 }
@@ -196,5 +226,49 @@ fn solve_part1(input_path: &Path) -> Result<SolutionExecution, String> {
 fn solve_part2(input_path: &Path) -> Result<SolutionExecution, String> {
     trace!("Running part 2...");
 
-    Ok(SolutionExecution::NotImplemented)
+    let file_data = parse_file(input_path)?;
+    let mut queue = VecDeque::new();
+    let mut result = u64::MAX;
+
+    for seed_range in file_data.seed_ranges.iter() {
+        queue.push_back(*seed_range);
+    }
+
+    while let Some(seed_range) = queue.pop_front() {
+        let seed_start = seed_range.0;
+        let seed_length = seed_range.1;
+        let seed_end = seed_start + seed_length - 1;
+
+        let v0 = file_data.apply(seed_start)?;
+        let v1 = file_data.apply(seed_end)?;
+
+        if seed_length > 2
+            && v1.wrapping_sub(v0) != seed_length - 1
+            && v0.wrapping_sub(v1) != seed_length - 1
+        {
+            let midpoint = (seed_start + seed_end) / 2;
+            let offset = match seed_length % 2 {
+                0 => 1,
+                1 => 0,
+                _ => unreachable!(),
+            };
+
+            queue.push_back(SeedRange(seed_start, seed_length / 2));
+            queue.push_back(SeedRange(
+                midpoint + offset,
+                seed_length / 2 + seed_length % 2,
+            ));
+            continue;
+        }
+
+        if v0 < result {
+            result = v0;
+        }
+
+        if v1 < result {
+            result = v1;
+        }
+    }
+
+    Ok(SolutionExecution::Value(result))
 }
